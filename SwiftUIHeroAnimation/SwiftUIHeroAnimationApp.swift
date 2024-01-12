efaabde59eb5c026bf11138149b0eabe2f7d42a4
심상갑 <kappa2410@gmail.com>
2024-01-12 16:05:58 +0900

    Initial Commit

---
+++ SwiftUIHeroAnimation/SwiftUIHeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHeroAnimationApp.swift
+//  SwiftUIHeroAnimation
+//
+//  Created by Gab on 2024/01/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
